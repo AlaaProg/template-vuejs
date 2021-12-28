38fc27a029249bb9b06925f893da0a1b035888f9
alaaProg <alaa.21.iraq@gmail.com>
2021-12-29 02:48:41 +0300

    add animate and contact page

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -4,10 +4,10 @@
             <img class="rounded-full w-48 h-48 object-center object-fill" src="./../assets/ali_miracle.jpg" alt="">
             <h1 class="text-3xl w-64 mt-10 font-bold ">Ali Abdul Ghani</h1>  
             <div class="bg-blue-600 w-14 h-1 my-8"></div>
-            <h1 class="capitalize font-semibold text-gray-600 leading-5">
-                Programmer <span class="text-gray-400">&</span>
-                Software Activist <span class="text-gray-400">&</span>
-                administrator system 
+            <h1 class="capitalize font-semibold text-gray-600 leading-5 ">
+                Programmer <span class="text-gray-500 font-bold">,</span>
+                SysAdmin <span class="text-gray-500 font-bold">,</span> 
+                Free Software Activist 
             </h1>
         </div>
         <div class="bg-white text-center w-full rounded-b shadow-lg">
